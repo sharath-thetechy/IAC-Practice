9c7a94878620ad4078b9192bbc54a09ab73bb8bc
sharath-thetechy <sharathchandragupta193@gmail.com>
2026-02-25 18:37:48 +0530

    files

---
+++ shubham-pract/variable.tf
@@ -0,0 +1,20 @@
+variable "aws_dynamodb_table" {
+    default = "dynadb-123"
+  
+}
+
+variable "key" {
+    default = "terr-key"
+  
+}
+
+variable "ami" {
+    default = "ami-0b6c6ebed2801a5cb"
+  
+}
+
+variable "sg" {
+    default = "security-grp"
+  
+}
+
